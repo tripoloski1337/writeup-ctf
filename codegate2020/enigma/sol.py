726670542b4359f8c2f11f17483a9589f146dbd0
tripoloski1337 <arsalan.dp@gmail.com>
2020-03-27 08:45:13 +0700

    adding codegate 2020

---
+++ codegate2020/enigma/sol.py
@@ -0,0 +1,31 @@
+DON'T LET YOUR RIGHT HAND KNOW WHAT YOUR LEFT HAND DID
+5+,'( "2( )+-3 r-/:( :*,5 ',+1 1:*( )+3 "26( :*,5 5-d
+
+
+ONCE A HACKER IS AN ETERNAL HACKER
++,92 * :*9'23 -4 *, 2(23,*" :*9'23
+
+
+A HACKER WITHOUT PHILOSOPHY IS JUST AN EVIL COMPUTER GENIUS
+* :*9'23 1-(:+-( @:-"+4+@:) -4 ;_4( *, 2?-" 9+.@_(23 /2,-_4
+
+flag is :
+9+52/*(22020{:*9'234 *32 ,+( !+3, +,") -( -4 .*52}
+CODEGATE2020{HACKERS ARE NOT !ORN ONLY IT IS MADE}
+
+
+
+{
+    A:"*",
+    C:"9",
+    D:"5",
+    O:"+",
+    M:".",
+    G:"/",
+    R:"3",
+    E:"2",
+    V:"?",
+    I:"-",
+    L:'"'
+
+}
